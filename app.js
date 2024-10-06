d69cf20c5b495b13605f49d2d03129b9b50754bf
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-10-06 20:51:52 +0330

    Set default middlewares

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
+const authRouter = require("./routes/v1/auth");
 
 const app = express();
+
+app.use("/courses/covers", express.static(path.join(__dirname, "/public", "courses", "covers")));
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(helmet());
 
+app.use("/v1/auth", authRouter);
 
 module.exports = app;
