d76aa5959574b7aa4861cac044bc5b7cf1598b09
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-10-08 15:07:48 +0330

    auth user ban route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,14 @@ const userModel = require("./../models/User");
 
 module.exports = async (req, res, next) => {
     const bearerToken = req.header("Authorization");
-    const bearerArray = bearerToken.split(" ");
+    const bearerArray = bearerToken?.split(" ");
 
-    if (bearerArray.length !== 2) return res.status(403).send("Not authorized");
+    if (bearerArray?.length !== 2) return res.status(403).send("Not authorized");
 
     const accessToken = bearerArray[1];
 
     try {
-        const verifyToken = jwt.verify(accessToken, process.env.SECRET_KEY);
+        const verifyToken = jwt.verify(accessToken, process.env.JWT_SECRET);
         const {id} = verifyToken;
 
         req.user = await userModel.findById(id, "-password -__v -createdAt -updatedAt").lean();
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const userController = require("./../../controllers/v1/user");
+const authMiddleware = require("./../../middlewares/auth");
+const isAdminMiddleware = require("./../../middlewares/isAdmin");
 
 const router = express.Router();
 
-router.post("/ban/:id", userController.banUser);
+router.route("/ban/:id")
+    .post(authMiddleware, isAdminMiddleware, userController.banUser);
 
 module.exports = router;
