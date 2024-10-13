0ce5e9a768068fb8857cc19caabf94b6593eba60
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-10-13 23:33:00 +0330

    Config nodemailer

--- modules/contact/contact.controller.js
+++ modules/contact/contact.controller.js
@@ -1,4 +1,5 @@
 const {isValidObjectId} = require("mongoose");
+const nodemailer = require("nodemailer");
 const contactModel = require("./Contact.model");
 const contactValidator = require("./contact.validator");
 
@@ -58,4 +59,52 @@ exports.remove = async (req, res) => {
 };
 
 exports.answer = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {answer} = req.body;
+
+        const isValidContactID = isValidObjectId(id);
+        if (!isValidContactID) return res.status(422).json({message: "invalid contactID"});
+
+        if (!answer) return res.status(422).json({message: "invalid answer"});
+
+        const contact = await contactModel.findOne({_id: id});
+        if (!contact) return res.status(404).json({message: "contact not found"});
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_ADDRESS,
+            to: contact.email,
+            subject: "this is a test email",
+            text: answer,
+        };
+
+        await transporter.sendMail(mailOptions, async (err, info) => {
+            if (err) {
+                console.log(`nodemailer error => ${err}`);
+                return res.status(500).json(err);
+            }
+
+            await contactModel.findOneAndUpdate({
+                _id: contact._id
+            }, {
+                answer: true,
+            });
+
+            return res.json({
+                message: "email send successfully",
+            });
+        });
+
+    } catch (err) {
+        console.log(`contact controller, answer error => ${err}`);
+        return res.status(500).json(err);
+    }
 };
